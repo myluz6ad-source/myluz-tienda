9505ed4fb58622892526b10c4af86abc6e35f756
myluz6ad-source <myluz6ad@gmail.com>
2026-02-19 01:02:59 -0400

    Create sw.js
    Signed-off-by: myluz6ad-source <myluz6ad@gmail.com>

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'bodega-myluz-v1';
+const ASSETS = [
+  'index.html',
+  'pedidos.html',
+  'pagos.html',
+  'manifest.json',
+  'assets/img/logo.ico',
+  'assets/img/logo.png'
+];
+
+// Instalar el Service Worker y guardar archivos base en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Escuchar peticiones y servir desde caché si no hay internet
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
